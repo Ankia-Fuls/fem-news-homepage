7919170338a571a64897611c0f7ca965eb2ea04a
Ankia-Fuls <ankiafuls01@gmail.com>
2025-10-07 15:54:18 +0200

    Commenting JS and CSS

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 const btnOpen = document.getElementById("btnOpen");
 const btnClose = document.getElementById("btnClose");
-const media = window.matchMedia("(width<500px)");
 const navbarMenu = document.querySelector(".navbar__menu");
 const main = document.querySelector("main");
 
+//screen width
+const media = window.matchMedia("(width<500px)");
+
+//inert sets the component inaccessibile to screen readers and tabbing
 function setupTopNav(e) {
     if (e.matches) {
         //is mobile
-        console.log("Mobile");
         navbarMenu.setAttribute("inert", "");
-        navbarMenu.style.transition = "none";       //removes the flash of the side nav when going from large to small screen
+        navbarMenu.style.transition = "none";       //adds a style attribute in the tag with transition none to overwrite the transistion in the styles, removing the transition
     }
     else {
         //desktop
-        console.log("Desktop");
         navbarMenu.removeAttribute("inert");
-        closeMobileMenu();
+        closeMobileMenu();                          //makes sure that if the screen changes to desktop while the navbar menu is open, it closes
     }
 }
 
 function openMobileMenu() {
     btnOpen.setAttribute("aria-expanded", "true");
     navbarMenu.removeAttribute("inert");
-    navbarMenu.removeAttribute("style");
-    main.setAttribute("inert", "");
-    btnClose.focus();
+    navbarMenu.removeAttribute("style");            //removes the transition style attribute, making the original transition take place again
+    main.setAttribute("inert", "");                 //cant focus or access the page below the menu
+    btnClose.focus();                               //auto focus on the close button for easier navigation
 }
 
 function closeMobileMenu() {
@@ -33,17 +34,17 @@ function closeMobileMenu() {
     main.removeAttribute("inert");
 
     setTimeout(() => {
-        navbarMenu.style.transition = "none";
+        navbarMenu.style.transition = "none";       //remove the transition again after a delay to make sure the transition has completed
     }, 500);
 
     btnOpen.focus();
 }
 
-setupTopNav(media);
+setupTopNav(media);                 //run once on page load and then when changes between screens
+media.addEventListener("change", (e) => {
+    setupTopNav(e);
+})
 
 btnOpen.addEventListener("click", openMobileMenu);
 btnClose.addEventListener("click", closeMobileMenu);
 
-media.addEventListener("change", (e) => {
-    setupTopNav(e);
-})
